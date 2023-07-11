adcb607a0bfd3dd812a4068c86af645b1a07c234
Hector Salas Serrano <kenkira666@gmail.com>
2023-07-11 07:46:49 -0500

    games done

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LINQ
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           List<string> VideoGames = new List<string>() {"Horizon Zero Down" , "Resident Evil 2", "Crash Bandicoot 4" };
+            VideoGames.Add("Attack on Titan");
+            VideoGames.Add("Gollum");
+
+            var orderVideoGames = VideoGames.OrderBy(name => name.Length);
+
+            foreach (var videoGame in orderVideoGames) 
+            {
+                Console.WriteLine(videoGame);
+            
+            }
         }
     }
 }
